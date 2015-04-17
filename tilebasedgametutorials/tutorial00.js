289f062cb6119ec6e02b71c5447ca2f641a880f2
Frank Poth <userpoth@gmail.com>
2015-04-17 19:29:31 -0400

    Redit first tutorial: better image loading, better explaination

--- tilebasedgametutorials/tutorial00.js
+++ tilebasedgametutorials/tutorial00.js
@@ -15,6 +15,7 @@
 		/* FUNCTIONS. */
 		///////////////
 
+		/* Draws the buffer to the display. */
 		function draw() {
 			/* Clear the display canvas. */
 			display.fillRect(0, 0, display.canvas.width, display.canvas.height);
@@ -24,6 +25,28 @@
 
 			/* Draw the buffer to the display canvas. */
 			display.drawImage(buffer.canvas, 0, 0, buffer.canvas.width, buffer.canvas.height, 0, 0, display.canvas.width, display.canvas.height);
+		}
+
+		/* This loads images and performs a callback function when finished. */
+		/* image_ is the image to set the src url_ on and callback_ is the function to execute once loading is finished. */
+		function startLoadImage(image_, url_, callback_) {
+			image_.addEventListener("load", loadImage);
+			image_.src = url_;
+			/* Handles an error should one occur. */
+			function errorImage(event_) {
+				/* This refers to the image being loaded or the target of the event_. */
+				this.removeEventListener("error", errorImage);
+				this.removeEventListener("load", loadImage);
+				alert("An error occurred while loading an image file.");
+				/* How's that for awesome error handling? Show stoppingly poor. */
+			}
+
+			/* Handles image loading. */
+			function loadImage(event_) {
+				this.removeEventListener("error", errorImage);
+				this.removeEventListener("load", loadImage);
+				callback_();
+			}
 		}
 
 		/* Having a resize event handler is always important when designing stuff for mobile devices because your browser window will most likely resize often. */
@@ -42,10 +65,11 @@
 				/* Remember, CSS doesn't like floating point values, so round down. */
 				html.display.style.left = Math.floor((client_width - client_height) * 0.5) + "px";
 			}
-			
+
 			/* The display canvas and its context have their values reset when it is resized, so you have to reset fillStyle whenever this happens. */
 			/* Drawing operations are super expensive in HTML5, especially with drawImage, so you want to use these things as sparingly as possible. */
-			display.fillStyle="#ffffff";
+			/* It makes no difference in this example because we only draw on resize events, but if you're drawing often, like in a game loop, you should avoid resetting variables that haven't changed. */
+			display.fillStyle = "#ffffff";
 			/* Call draw to draw the map to the display canvas. */
 			draw();
 		}
@@ -53,13 +77,12 @@
 		///////////////////////
 		/* OBJECT LITERALS. */
 		/////////////////////
+		/* I like keeping these separate from my functions and variables. */
 
-		/* The html object holds references to actual html elements. */
+		/* The html object holds references to actual html elements defined in the html document. */
 		var html = {
 			/* Gets the canvas element from the html. */
 			display : document.getElementById("display"),
-			/* Gets the img element from the html. */
-			image : document.getElementById("image")
 		};
 
 		/* The map object holds information about the map. */
@@ -75,15 +98,19 @@
 					var value = this.values[index];
 
 					/* You only want to draw if your value is == 1. */
-					/* If you want to draw to every tile space on the map, just forget about this if statement. */
+					/* If you want to draw to every tile space on the map (like in a top down scroller), just forget about this if statement. */
 					if (value == 1) {
 						/* These are really simple calculations. */
-						var destination_x = (index % this.columns) * 16;
-						var destination_y = Math.floor(index / this.columns) * 16;
+						/* Derive the row and column of the tile to draw from the index in the map array. */
+						var column = index % this.columns;
+						var row = Math.floor(index / this.columns);
+						/* Multiply by 16 to get its physical location on screen. */
+						var destination_x = column * 16;
+						var destination_y = row * 16;
 
 						/* The source image is 16x16px. */
 						/* You blit from the source image at 0x, 0y, 16width, 16height, and then to the destination, which is the buffer. */
-						buffer.drawImage(html.image, 0, 0, 16, 16, destination_x, destination_y, 16, 16);
+						buffer.drawImage(image, 0, 0, 16, 16, destination_x, destination_y, 16, 16);
 					}
 				}
 			},
@@ -107,23 +134,36 @@
 		/* On each screen update, you draw several things to the buffer and then draw the buffer once to the display. */
 		/* If you were to skip drawing to the buffer, you might see some flicker on the display canvas because the different drawing operations do not all happen at once, but rather one at a time. */
 		/* The buffer ensures that everything you see on screen is drawn at once, thus eliminating the chance of flicker. */
+		/* Also, I scale the buffer to fit the display canvas as the size of the display canvas may change. */
+		/* It's much better to scale one buffer than 256 individual tiles (drawImage is painfully slow, moreso in the call than the actual blitting of small images). */
 		var buffer = document.createElement("canvas").getContext("2d");
+
 		/* The drawing context of the display canvas element. */
 		var display = html.display.getContext("2d");
+
+		/* The tile image. The source image file still needs to be loaded in, though. */
+		var image = new Image();
 
 		//////////////////
 		/* INITIALIZE. */
 		////////////////
-		/* I guess tutorial00() is really the initialize function, but this is where the program is launched into action. */
 
 		/* Set the dimensions of the buffer to match your map. */
-		/* You have 16 rows by 16 columns at 16px width and height, so 256px by 256 px. */
+		/* You have 16 rows by 16 columns at 16px width and height, so 256px by 256px. */
 		buffer.canvas.height = buffer.canvas.width = 256;
 
 		/* Listen for resize events. */
 		window.addEventListener("resize", resizeWindow);
 
-		/* Make sure the display canvas is sized properly. */
-		/* I've noticed that some browsers initialize with invalid window dimensions, and some even fire the resize event when starting up, so it's best to add the event listener and call the function just to be safe. */
-		resizeWindow();
+		/* Here's where we load the image. */
+		/* The callback function will execute once the image is done loading. */
+		/* It's important not to call resizeWindow before the image has loaded, because it also does the initial drawing of the tile map. */
+		/* If you call it before the image loads, you won't see anything until the screen is resized again after the image finishes loading. */
+		startLoadImage(image, "tutorial00.png", function() {
+			/* Make sure the display canvas is sized properly. */
+			/* I've noticed that some browsers initialize with invalid window dimensions, and some fire the resize event at different times than others when starting up, so it's best to add the event listener and call the function just to be safe. */
+			resizeWindow();
+		});
 	})();
+
+/* And that's it! If you take out all the comments, you have a pretty small bit of code that draws a tilemap to the screen. */
